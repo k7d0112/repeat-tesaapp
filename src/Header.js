a15522dd1d30a07c2cb4b4508fdab6f3f281a69d
John Doe <ka7de0112@gmail.com>
2024-07-15 22:11:29 +0900

    課題1実装

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+export default function Header (){
+    return (
+      <header className='p-6 bg-[#333] flex justify-between  items-center'>
+        <h1>
+          <a href='' className='font-sans text-white font-bold'>Blog</a>
+        </h1>
+        <a href='' className='font-sans text-white font-bold'>お問い合わせ</a>
+      </header>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/Header.js',
+    './src/Blog.js',
+    './src/BlogDetail.js',
+    './src/Contact.js',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
