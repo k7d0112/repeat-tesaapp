fb7e66442484d45035fcc9e4c3ddacc38210be5b
John Doe <ka7de0112@gmail.com>
2024-07-16 09:58:35 +0900

    課題2実装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
-import Blog from './Blog';
+import BlogList from './Blog';
+import BlogDetailShow from './BlogDetail';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
-      <Blog />
+      <Routes>
+        <Route path='/' element={<BlogList />} />
+        <Route path='/BlogDetail/:id' element={<BlogDetailShow />} />
+        {/* <Route path='/Contact' element={<Contact />} /> */}
+      </Routes>
     </>
   );
 }
